07f4fce3294e20c9f28d174a587bded08e55cdba
iam-Abol <mh.z77w@gmail.com>
2021-03-01 13:39:44 +0330

    The3n+1Problem solution changed :-)

--- Online Judge/The3n+1Problem/The3n+1Problem.cpp
+++ Online Judge/The3n+1Problem/The3n+1Problem.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
-int f(int n){
-	int r = 1;
-	while (n != 1)
-	{
-		if (n % 2 == 1)
-			n = 3 * n + 1;
-		else
-			n /= 2;
-		r++;
-	}
-	return r;
-}
+int f(int n)
+{
+	int i = 1;
 
+	while (n != 1){
+		if (n % 2 == 0){
+			n = n / 2;
+		}
+		else{
+			n = 3 * n + 1;
+		}
+		i++;
+	}
+	return i;
+}
 int main()
 {
 	int i, j;
-	std::cin >> i >> j;
-	std::cout << i << " " << j << " ";
-	int max = 0;
-	for (int k = i; k <= j; k++)
-	{
-		int m = f(k);
-		if (m > max)
-			max = m;
-	}
-	std::cout << max << std::endl;
 
-	return 0;
+	while (std::cin >> i >> j){
+		std::cout << i << " " << j << " ";
+		if (i>j){
+			int t = i;
+			i = j;
+			j = t;
+		}
+		int max = f(i);
+		for (int k = i + 1; k <= j; k++) {
+			int temp = f(k);
+			if (temp > max)
+				max = temp;
+		}
+		std::cout << max << std::endl;
+	}
+	return(0);
 }
 
